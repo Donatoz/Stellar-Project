14def0b63c9462259afc01709c90d5c5f76fa4f5
kigris <kigris@ttu.ee>
2021-02-12 17:41:00 +0200

    Separated color extensions

---
+++ System/Extensions/ColorExtensions.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Metozis.System.Extensions
+{
+    public static class ColorExtensions
+    {
+        public static Color HDR(this Color c, float intensity)
+        {
+            var factor = Mathf.Pow(2, intensity);
+            return new Color(c.r * factor, c.g * factor, c.b * factor, c.a);
+        }
+
+        public static Color LerpToWhite(this Color c, float amount)
+        {
+            return Color.Lerp(c, Color.white, amount);
+        }
+
+        public static Color Alpha(this Color c, float a)
+        {
+            return new Color(c.r, c.g, c.b, a);
+        }
+        
+        public static Color LerpToBlack(this Color c, float amount)
+        {
+            return Color.Lerp(c, Color.black, amount);
+        }
+    }
+}
--- System/Extensions/UnityExtensions.cs
+++ System/Extensions/UnityExtensions.cs
@@ -4,17 +4,6 @@ namespace Metozis.System.Extensions
 {
     public static class UnityExtensions
     {
-        public static Color HDR(this Color c, float intensity)
-        {
-            var factor = Mathf.Pow(2, intensity);
-            return new Color(c.r * factor, c.g * factor, c.b * factor, c.a);
-        }
-
-        public static Color LerpToWhite(this Color c, float amount)
-        {
-            return Color.Lerp(c, Color.white, amount);
-        }
-
         public static Vector3 UniformlyRandomizedBy(this Vector3 v, Vector2 value)
         {
             var randomVal = Random.Range(value.x, value.y);
